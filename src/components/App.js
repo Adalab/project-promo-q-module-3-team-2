5776d6854794bb030e773e480dd4d99230cd6e37
Blanca <blancabcbt@gmail.com>
2022-08-18 14:21:56 +0200

    add arrows

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ function App() {
     /* photo: "../images/img-preview.png", */
   });
   const [classCollapsed, setClassCollapsed] = useState("collapsed");
+  const [arrowRotated, setArrowRotated] = useState("");
 
   const handlerInput = (ev) => {
     const inputValue = ev.target.value;
@@ -31,13 +32,25 @@ function App() {
   const handleCollapsable = (ev) => {
     const menuClicked = ev.currentTarget.id;
     descolapsar(menuClicked);
+    rotate (menuClicked);
     console.log(menuClicked);
+    
+
   }; 
+
+  const rotate =(id) =>{
+    arrowRotated === ""
+       ? setArrowRotated("rotate")
+       : setArrowRotated("");
+  }
   function descolapsar(id) {
-      classCollapsed === "collapsed"
-        ? setClassCollapsed("")
-        : setClassCollapsed("collapsed");
+        classCollapsed === "collapsed"
+       ? setClassCollapsed("")
+       : setClassCollapsed("collapsed");
+
     }
+
+
 
 const reset = () =>{
   setDataCard(  { pallete: "1",
@@ -66,16 +79,16 @@ const reset = () =>{
       </header>
       <main className="mainCard">
         <section className="preview">
-          <button className="preview__reset js_btnReset" onClick={reset}>
+          <button className="preview__reset " onClick={reset}>
             <i className="fa-regular fa-trash-can"></i> Reset
           </button>
 
-          <article className={`preview__box js_previewBox palette-${dataCard.pallete}`}>
-            <h3 className="preview__name js_previewName">
+          <article className={`preview__box palette-${dataCard.pallete}`}>
+            <h3 className="preview__name ">
               {dataCard.name || `Nombre Apellido`}
             </h3>
-            <p className="preview__job js_previewJob">{dataCard.job || `Front-end developer`}</p>
-            <div className="preview__photo js__profile-image"></div>
+            <p className="preview__job ">{dataCard.job || `Front-end developer`}</p>
+            <div className="preview__photo"></div>
 
             <nav>
               <ul className="preview__socialIcons">
@@ -84,10 +97,10 @@ const reset = () =>{
                     href={`tel:${dataCard.phone}`}
                     
                     rel="noreferrer"
-                    className="preview__icon js_previewPhone js_preview_icon"
+                    className="preview__icon"
 
                   >
-                    <i className="fa-solid fa-mobile-screen-button js_icons"></i>
+                    <i className="fa-solid fa-mobile-screen-button"></i>
                   </a>
                 </li>
                 <li>
@@ -95,9 +108,9 @@ const reset = () =>{
                     href={`mailto:${dataCard.email}`}
                     
                     rel="noreferrer"
-                    className="preview__icon js_previewEmail js_preview_icon"
+                    className="preview__icon js_previewEmail"
                   >
-                    <i className="fa-regular fa-envelope js_icons"></i>
+                    <i className="fa-regular fa-envelope"></i>
                   </a>
                 </li>
 
@@ -106,9 +119,9 @@ const reset = () =>{
                     href={dataCard.linkedin}
                     target="_blank"
                     rel="noreferrer"
-                    className="preview__icon js_previewLinkedin js_preview_icon"
+                    className="preview__icon js_previewLinkedin "
                   >
-                    <i className="fa-brands fa-linkedin-in js_icons"></i>
+                    <i className="fa-brands fa-linkedin-in "></i>
                   </a>
                 </li>
                 <li>
@@ -116,21 +129,21 @@ const reset = () =>{
                     href={dataCard.github}
                     rel="noreferrer"
                     target="_blank"
-                    className="preview__icon js_previewGithub js_preview_icon"
+                    className="preview__icon"
                   >
-                    <i className="fa-brands fa-github-alt js_icons"></i>
+                    <i className="fa-brands fa-github-alt "></i>
                   </a>
                 </li>
               </ul>
             </nav>
-            <div className="preview__rectangle js_preview__rectangle"></div>
+            <div className="preview__rectangle"></div>
           </article>
         </section>
 
         <form className="form">
           <fieldset className="desing">
             <section
-              className="headSection  design js_designTitle"
+              className="headSection  design"
               onClick={handleCollapsable}
               id="design"
             >
@@ -138,14 +151,14 @@ const reset = () =>{
                 <i className="fa fa-object-ungroup" aria-hidden="true"></i>
                 Diseña
               </h2>
-              <i className="fa-solid fa-angle-down js_iconDesign rotate"></i>
+              <i className={`fa-solid fa-angle-down ${arrowRotated}`}></i>
             </section>
             <div className={`desing__div js_divDesign ${classCollapsed}`}>
               <h3 className="desing__div--title3">Colores</h3>
               <div className="desing__div--colors">
                 <label className="familyColor1" htmlFor="familyColor1">
                   <input
-                    className="js_inputRadio js_inputRadio1"
+                   
                     id="familyColor1 "
                     type="radio"
                     value="1"
@@ -159,7 +172,7 @@ const reset = () =>{
                 </label>
                 <label className="familyColor2" htmlFor="familyColor2">
                   <input
-                    className="js_inputRadio js_inputRadio2"
+                    
                     id="familyColor2"
                     type="radio"
                     value="2"
@@ -173,7 +186,7 @@ const reset = () =>{
                 </label>
                 <label className="familyColor3" htmlFor="familyColor3">
                   <input
-                    className="js_inputRadio js_inputRadio3"
+                    
                     id="familyColor3"
                     type="radio"
                     value="3"
@@ -188,16 +201,16 @@ const reset = () =>{
               </div>
             </div>
           </fieldset>
-          <fieldset className="fill js_allInputs">
+          <fieldset className="fill">
             <section
-              className="headSection js_fillTitle"
+              className="headSection"
               onClick={handleCollapsable}
               id="fill"
             >
               <h2 className=" headSection__title">
                 <i className="fa fa-keyboard" aria-hidden="true"></i>Rellena
               </h2>
-              <i className="fa-solid fa-angle-down js_iconFill"></i>
+              <i className={`fa-solid fa-angle-down ${arrowRotated}`}></i>
             </section>
             <div className={`fillInput  ${classCollapsed}`}>
               <label className="fillInput__label" htmlFor="fullName">
@@ -237,17 +250,17 @@ const reset = () =>{
                   type="file"
                   name="photo"
                   id="img-selector"
-                  className="action__hiddenField js__profile-upload-btn"
+                  className="action__hiddenField"
                 />
 
-                <div className="profile__preview js__profile-preview"></div>
+                <div ></div>
               </div>
 
               <label className="fillInput__label" htmlFor="Email">
                 Email
               </label>
               <input
-                className="fillInput__input js_inputEmail"
+                className="fillInput__input"
                 type="mail"
                 id="Email"
                 name="email"
@@ -262,7 +275,7 @@ const reset = () =>{
                 Teléfono
               </label>
               <input
-                className="fillInput__input js_inputPhone"
+                className="fillInput__input"
                 type="tel"
                 id="tel"
                 name="phone"
@@ -276,7 +289,7 @@ const reset = () =>{
                 Linkedin
               </label>
               <input
-                className="fillInput__input js_inputLinkedin"
+                className="fillInput__input "
                 type="url"
                 id="linkedin"
                 name="linkedin"
@@ -290,7 +303,7 @@ const reset = () =>{
                 Github
               </label>
               <input
-                className="fillInput__input js_inputGithub"
+                className="fillInput__input"
                 type="url"
                 id="github"
                 name="github"
@@ -303,24 +316,24 @@ const reset = () =>{
           </fieldset>
           <fieldset className="share">
             <section
-              className="headSection js_fillTitle"
+              className="headSection"
               onClick={handleCollapsable}
               id="share"
             >
               <h2 className=" headSection__title">
                 <i className="fa fa-share-nodes" aria-hidden="true"></i>Comparte
               </h2>
-              <i className="fa-solid fa-angle-down js_iconShare"></i>
+              <i className={`fa-solid fa-angle-down ${arrowRotated}`}></i>
             </section>
             <section className={`${classCollapsed}`}>
-              <button className="share__button js_shareButton">
+              <button className="share__button">
                 <i className="fa-regular fa-address-card share__button--icon"></i>
                 Crear tarjeta
               </button>
 
               <div className="card  collapsed">
-                <p className="card__text js_msj"></p>
-                <a href="" className="card__link js_url" target="_blank"  rel="noreferrer">
+                <p className="card__text"></p>
+                <a href="" className="card__link " target="_blank"  rel="noreferrer">
                   {" "}
                 </a>
 
@@ -328,7 +341,7 @@ const reset = () =>{
                   <i className="fa-brands fa-twitter card__twitter--icon"></i>
 
                   <a
-                    className="card__twitter--link js_shareTwitter"
+                    className="card__twitter--link"
                     href=""
                     target="_blank"
                     rel="noreferrer"
