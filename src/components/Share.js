03e4c04a705a31dab0e10171b23f54b0914d1b8d
NadiaMonzon <nadiamonque99@gmail.com>
2022-09-01 13:02:19 +0100

    Change share button color

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,4 +1,5 @@
 import callToApi from "../services/fetch";
+import '../styles/Share.scss'
 import { useState } from "react";
 
 const Share = (props) => {
@@ -19,12 +20,12 @@ const Share = (props) => {
   const renderErrorMsg=()=>{
     if(props.shareLink.success){
       return (<>
-        <a href={props.shareLink.cardURL}>{props.shareLink.cardURL}</a>
+        <a className='card__herokulink' href={props.shareLink.cardURL}>{props.shareLink.cardURL}</a>
         <div className="card__twitter">
-          <i className="fa-brands fa-twitter card__twitter--icon"></i>{" "}
+          <i className="fa-brands fa-twitter card__twitter--icon"></i>
           <a
             className="card__twitter--link"
-            href={`https://twitter.com/intent/tweet?text=He%20creado%20mi%20mi%20tarjeta%20con%20Awsome%20Profile&url=${props.shareLink.cardURL}`}
+            href={`https://twitter.com/intent/tweet?text=He%20creado%20mi%20mi%20tarjeta%20con%20Awesome%20Profile&url=${props.shareLink.cardURL}`}
             target="_blank"
             rel="noreferrer"
             title="Comparte la tarjeta creada en Twitter"
@@ -43,6 +44,7 @@ const Share = (props) => {
     ev.preventDefault();
     validateForm();
     renderErrorMsg();
+    
     callToApi(props.dataCard).then((link) => {
       props.handleShare(link);
     }); 
@@ -56,15 +58,12 @@ const Share = (props) => {
         </h2>
         <i className={`fa-solid fa-angle-down ${props.isOpen? '' : 'rotate'}`}></i>
       </section>
-      <section className={`${props.isOpen? '' : 'collapsed'}`}>
-        <button className="share__button" onClick={handleShare} title='Crea la tarjeta con los datos que has introducido'>
+      <section className={`${props.isOpen? '' : 'collapsed'} share__container`}>
+        <button className={`share__button`} onClick={handleShare} title='Crea la tarjeta con los datos que has introducido'>
           <i className="fa-regular fa-address-card share__button--icon"></i>
           Crear tarjeta
         </button>
-
-        <div className="card ">
           {renderErrorMsg()}         
-        </div>
       </section>
     </fieldset>
   );
