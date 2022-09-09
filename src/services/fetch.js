e2a20cce221b7bb1cc6c1b5539c5609a407f7897
vvarona <vvaronad@gmail.com>
2022-09-09 14:08:09 +0200

    merge and change fetch url

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,6 +1,6 @@
 const callToApi = (data) => {
   // Llamamos a la API
-  return fetch("http://localhost:4000/card/", {
+  return fetch("https://las-faraonas-awesome-card.herokuapp.com/card/", {
     method: "POST",
     body: JSON.stringify(data),
     headers: { "content-type": "application/json" },
